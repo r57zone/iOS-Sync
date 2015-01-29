f5f7049cc1de211aa227142fbf868c01317954a5
r57zone <r57zone@gmail.com>
2015-01-29 18:35:52 +0400

    'update'

--- Unit1.pas
+++ Unit1.pas
@@ -37,7 +37,6 @@ type
     procedure DoOnDeviceDisconnect(Sender: TObject;Device: TAMoblieDevice);
     procedure Dir(Address:string);
     procedure DoOnFileTransStep(Sender: TObject;Step: Cardinal);
-    procedure StatusBar(SimpleText:string);
     { Private declarations }
   public
     { Public declarations }
@@ -51,10 +50,6 @@ var
 implementation
 
 {$R *.dfm}
-
-function empty_func:boolean;
-begin
-end;
 
 procedure TForm1.DoOnDeviceConnect(Sender: TObject;
   Device: TAMoblieDevice);
@@ -72,19 +67,8 @@ Dir(Path);
 end;
 end;
 
-function GetSystemLanguage:string;
-var
-Buffer:PChar;
-Size:integer;
+function EmptyFunc:boolean;
 begin
-Size:=GetLocaleInfo (LOCALE_USER_DEFAULT, LOCALE_SENGLANGUAGE, nil, 0);
-GetMem(Buffer, Size);
-try
-GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SENGLANGUAGE, Buffer, Size);
-result:=Buffer;
-finally
-FreeMem(Buffer);
-end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -104,7 +88,7 @@ lpAMobileDeviceModule:=TAMobileDeviceModule.Create;
 lpAMobileDeviceModule.OnDeviceConnect:=DoOnDeviceConnect;
 lpAMobileDeviceModule.OnDeviceDisconnect:=DoOnDeviceDisconnect;
 if lpAMobileDeviceModule.InitialModule then Connected:=true;
-if lpAMobileDeviceModule.Subscribe then empty_func;
+if lpAMobileDeviceModule.Subscribe then EmptyFunc;
 end;
 ListView1.Columns[0].Width:=ListView1.Width-30;
 //ListView1.Perform(LVM_SETCOLUMNWIDTH, 0, 200);
@@ -131,9 +115,9 @@ begin
 ListCompleted:=false;
 ListView1.Clear;
 
-if trim(address)='' then address:='/';
+if Trim(Address)='' then Address:='/';
 
-if (address<>'/') then begin
+if (Address<>'/') then begin
 Item:=ListView1.Items.Add;
 Item.Caption:='.';
 Item.SubItems.Add('');
@@ -146,21 +130,21 @@ Item.ImageIndex:=0;
 end;
 
 DirList:=TStringList.Create;
-FDevice.GetDirectories(AnsiToUTF8(address),DirList);
+FDevice.GetDirectories(AnsiToUTF8(Address),DirList);
 
 for i:=0 to DirList.Count-1 do begin
 Item:=ListView1.Items.Add;
-Item.Caption:=StringReplace(UTF8ToAnsi(DirList.Strings[i]),'и?','й',[rfreplaceall]);
-Item.Caption:=StringReplace(Item.Caption,'И?','Й',[rfreplaceall]);
+Item.Caption:=StringReplace(UTF8ToAnsi(DirList.Strings[i]),'и?','й',[rfReplaceAll]);
+Item.Caption:=StringReplace(Item.Caption,'И?','Й',[rfReplaceAll]);
 //Item.SubItems.Add('');
 Item.ImageIndex:=2;
 end;
 DirList.Clear;
-FDevice.GetFiles(AnsiToUTF8(address),DirList);
+FDevice.GetFiles(AnsiToUTF8(Address),DirList);
 for i:=0 to DirList.Count-1 do begin
 Item :=ListView1.Items.Add;
-Item.Caption:=StringReplace(DirList.Strings[i],'и?','й',[rfreplaceall]);
-Item.Caption:=StringReplace(Item.Caption,'И?','Й',[rfreplaceall]);
+Item.Caption:=StringReplace(DirList.Strings[i],'и?','й',[rfReplaceAll]);
+Item.Caption:=StringReplace(Item.Caption,'И?','Й',[rfReplaceAll]);
 //Item.SubItems.Add('');
 Item.SubItems.Add('');
 Item.ImageIndex:=4;
@@ -170,7 +154,7 @@ if (ExtractFileExt(AnsiLowerCase(DirList.Strings[i]))='.jpg') or (ExtractFileExt
 or (ExtractFileExt(AnsiLowerCase(DirList.Strings[i]))='.png') or (ExtractFileExt(AnsiLowerCase(DirList.Strings[i]))='.gif') then Item.ImageIndex:=6;
 end;
 DirList.Free;
-Path:=address;
+Path:=Address;
 ListCompleted:=true;
 end;
 
@@ -180,9 +164,9 @@ begin
 if ListView1.ItemIndex<>-1 then begin
 if Button=MbLeft then begin
 case ListView1.Items[ListView1.ItemIndex].ImageIndex of
-1: dir('/');
-0: if Path<>'/' then begin delete(Path,pos(ExtractFileName(StringReplace(Path,'/','\',[rfreplaceall])),Path)-1,length(ExtractFileName(StringReplace(Path,'/','\',[rfreplaceall])))+1); Dir(Path); end;
-2: if Path='/' then dir(Path+ListView1.Items[ListView1.ItemIndex].Caption) else dir(Path+'/'+ListView1.Items[ListView1.ItemIndex].Caption);
+1: Dir('/');
+0: if Path<>'/' then begin Delete(Path,pos(ExtractFileName(StringReplace(Path,'/','\',[rfReplaceAll])),Path)-1,length(ExtractFileName(StringReplace(Path,'/','\',[rfReplaceAll])))+1); Dir(Path); end;
+2: if Path='/' then Dir(Path+ListView1.Items[ListView1.ItemIndex].Caption) else Dir(Path+'/'+ListView1.Items[ListView1.ItemIndex].Caption);
 end;
 end;
 if Button=MbRight then begin
@@ -199,44 +183,44 @@ var
 AMobileFile:TAMobileDeviceFileStream;
 LocalFile:TFileStream;
 begin
-if (address<>'/') and (Connected=true) then begin
+if Connected then begin
 LocalFile:=TFileStream.Create(Local,fmOpenRead);
 LocalFile.Position:=0;
 try
-if Form1.FDevice.Exists(AnsiToUtf8(address+'/'+ExtractFileName(Local))) then
-Form1.FDevice.DeleteFile(AnsiToUtf8(address+'/'+ExtractFileName(Local)));
+if Form1.FDevice.Exists(AnsiToUtf8(Address+'/'+ExtractFileName(Local))) then
+Form1.FDevice.DeleteFile(AnsiToUtf8(Address+'/'+ExtractFileName(Local)));
 
 AMobileFile:=TAMobileDeviceFileStream.Create(Form1.FDevice,AnsiToUtf8(Address+'/'+ExtractFileName(Local)),omWrite);
 AMobileFile.OnProgressStep:=Form1.DoOnFileTransStep;
 AMobileFile.CopyFrom(LocalFile,LocalFile.Size);
 AMobileFile.Destroy;
-result:=true;
+Result:=true;
 except
 //on E:Exception do
 //ShowMessage(Local+'/'+ExtractFileName(StrPas(Filename))+ '  ' + E.Message);
-result:=false;
+Result:=false;
 end;
 LocalFile.Destroy;
-end else result:=false;
+end else Result:=false;
 end;
 
-function UploadDir(LocalPath,address:string):boolean;
+function UploadDir(LocalPath,Address:string):boolean;
 var
 SR:TSearchRec;
 begin
 try
-Form1.FDevice.CreateDirectory(AnsiToUTF8(address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1))));
+Form1.FDevice.CreateDirectory(AnsiToUTF8(Address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1))));
 
 if FindFirst(LocalPath + '*.*', faAnyFile, SR) = 0 then begin
 repeat
-if (SR.Attr<>faDirectory) then UploadFile(LocalPath+SR.Name,address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1)));
+if (SR.Attr<>faDirectory) then UploadFile(LocalPath+SR.Name,Address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1)));
 until FindNext(SR)<>0;
 FindClose(SR);
 end;
 
 if FindFirst(LocalPath + '*', faAnyFile, SR) = 0 then begin
 repeat
-if (SR.Attr=faDirectory) and (SR.Name<>'.') and (SR.Name<>'..') then UploadDir(LocalPath+SR.Name+'\',address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1)));
+if (SR.Attr=faDirectory) and (SR.Name<>'.') and (SR.Name<>'..') then UploadDir(LocalPath+SR.Name+'\',Address+'/'+ExtractFileName(Copy(LocalPath,1,Length(LocalPath)-1)));
 until FindNext(SR)<>0;
 FindClose(SR);
 end;
@@ -263,13 +247,13 @@ for i:=0 to (Amount - 1) do begin
 Size:=DragQueryFile(Msg.WParam, i, nil, 0) + 1;
 Filename:=StrAlloc(Size);
 DragQueryFile(Msg.WParam, i, Filename, Size);
-Inc(Count);
+inc(Count);
 
 StatusBar1.SimpleText:=' Идет копирование файлов ('+IntToStr(i+1)+' из '+IntToStr(Amount)+')';
 
 if FileExists(StrPas(Filename)) then begin
-if UploadFile(StrPas(Filename),Path) then Inc(GoodCount); end else
-if DirectoryExists(StrPas(Filename)) then if UploadDir(StrPas(Filename)+'\',Path) then Inc(GoodCount);
+if UploadFile(StrPas(Filename),Path) then inc(GoodCount); end else
+if DirectoryExists(StrPas(Filename)) then if UploadDir(StrPas(Filename)+'\',Path) then inc(GoodCount);
 
 StrDispose(Filename);
 end;
@@ -287,27 +271,27 @@ begin
 Application.ProcessMessages;
 end;
 
-function RemoveDirAndFiles(address:string):boolean;
+function RemoveDirAndFiles(Address:string):boolean;
 var
 DirList:TStringList; i:integer;
 begin
 try
-address:=StringReplace(UTF8ToAnsi(address),'и?','й',[rfreplaceall]);
-address:=AnsiToUTF8(StringReplace(address,'И?','Й',[rfreplaceall]));
+Address:=StringReplace(UTF8ToAnsi(Address),'и?','й',[rfReplaceAll]);
+Address:=AnsiToUTF8(StringReplace(Address,'И?','Й',[rfReplaceAll]));
 
 DirList:=TStringList.Create;
 
-Form1.FDevice.GetFiles(address,DirList);
+Form1.FDevice.GetFiles(Address,DirList);
 for i:=0 to DirList.Count-1 do
-Form1.FDevice.DeleteFile(address+'/'+DirList.Strings[i]);
+Form1.FDevice.DeleteFile(Address+'/'+DirList.Strings[i]);
 
 DirList.Clear;
 
-Form1.FDevice.GetDirectories(address,DirList);
+Form1.FDevice.GetDirectories(Address,DirList);
 for i:=0 to DirList.Count-1 do
-if (DirList.Strings[i]<>'.') and (DirList.Strings[i]<>'..') then RemoveDirAndFiles(address+'/'+DirList.Strings[i]);
+if (DirList.Strings[i]<>'.') and (DirList.Strings[i]<>'..') then RemoveDirAndFiles(Address+'/'+DirList.Strings[i]);
 
-Form1.FDevice.DeleteDirectory(address,true);
+Form1.FDevice.DeleteDirectory(Address,true);
 DirList.Free;
 Result:=true;
 except
@@ -318,12 +302,12 @@ end;
 procedure TForm1.N11Click(Sender: TObject);
 begin
 if FileIndex[1]<>'2' then begin
-delete(FileIndex,1,1);
+Delete(FileIndex,1,1);
 if Form1.FDevice.DeleteFile(AnsiToUtf8(Path+'/'+ExtractFileName(FileIndex))) then begin
 if length(FileIndex)>27 then FileIndex:=copy(FileIndex,1,27)+'...';
 StatusBar1.SimpleText:=' Файл "'+FileIndex+'" успешно удален'; Dir(Path); end else StatusBar1.SimpleText:=' Не удается удалить файл "'+FileIndex+'"';
 end else begin
-delete(FileIndex,1,1);
+Delete(FileIndex,1,1);
 if RemoveDirAndFiles(AnsiToUtf8(Path+'/'+ExtractFileName(FileIndex))) then begin
 StatusBar1.SimpleText:=' Папка "'+FileIndex+'" успешно удалена'; Dir(Path); end else StatusBar1.SimpleText:=' Не удается удалить папку "'+FileIndex+'"';
 end;
@@ -331,14 +315,14 @@ end;
 
 procedure TForm1.N1Click(Sender: TObject);
 var
-namedir:string;
+NameDir:string;
 begin
-if InputQuery(Application.Title, 'Введите название папки', namedir) then
-if trim(namedir)<>'' then begin
-if Form1.FDevice.Exists(AnsiToUtf8(Path+'/'+namedir)) then StatusBar1.SimpleText:=' Такая папка уже существует' else begin
-Form1.FDevice.CreateDirectory(AnsiToUtf8(Path+'/'+namedir));
+if InputQuery(Application.Title, 'Введите название папки', NameDir) then
+if Trim(NameDir)<>'' then begin
+if Form1.FDevice.Exists(AnsiToUtf8(Path+'/'+NameDir)) then StatusBar1.SimpleText:=' Такая папка уже существует' else begin
+Form1.FDevice.CreateDirectory(AnsiToUtf8(Path+'/'+NameDir));
 Dir(Path);
-StatusBar1.SimpleText:=' Папка "'+namedir+'" успешно создана';
+StatusBar1.SimpleText:=' Папка "'+NameDir+'" успешно создана';
 end;
 end else StatusBar1.SimpleText:=' Неверно задано имя папки';
 end;
@@ -365,7 +349,7 @@ begin
 if PChar(TWMCopyData(msg).CopyDataStruct.lpData)='WORK' then SendMessageToHandle(FindWindow(nil,'PodCast Easy'),'YES');
 
 if copy(PChar(TWMCopyData(msg).CopyDataStruct.lpData),1,13)='FILES TO SYNC' then begin
-while not ListCompleted do begin sleep(15); Application.ProcessMessages; end;
+while not ListCompleted do begin Sleep(15); Application.ProcessMessages; end;
 Count:=0;
 GoodCount:=0;
 SyncList:=TStringList.Create;
@@ -375,8 +359,8 @@ for i:=0 to SyncList.Count-1 do begin
 inc(Count);
 StatusBar1.SimpleText:=' Идет копирование файлов ('+IntToStr(i+1)+' из '+IntToStr(SyncList.Count)+')';
 if FileExists(SyncList.Strings[i]) then begin
-if UploadFile(SyncList.Strings[i],path) then Inc(GoodCount); end else
-if DirectoryExists(SyncList.Strings[i]) then if UploadDir(SyncList.Strings[i]+'\',Path) then Inc(GoodCount);
+if UploadFile(SyncList.Strings[i],Path) then inc(GoodCount); end else
+if DirectoryExists(SyncList.Strings[i]) then if UploadDir(SyncList.Strings[i]+'\',Path) then inc(GoodCount);
 end;
 if Count=GoodCount then begin
 StatusBar1.SimpleText:=' Все файлы успешно загружены';
@@ -395,48 +379,48 @@ end;
 
 procedure TForm1.N3Click(Sender: TObject);
 var
-newname:string;
+NewName:string;
 begin
-if (FileIndex[1]<>'0') and (FileIndex[1]<>'1') then delete(FileIndex,1,1);
-if InputQuery(Application.Title, 'Введите новое название для '+FileIndex, newname) then
-if trim(newname)<>'' then begin
-if FDevice.Rename(AnsiToUtf8(Path+'/'+FileIndex),AnsiToUtf8(Path+'/'+newname)) then StatusBar1.SimpleText:=' Название успешно изменено' else StatusBar1.SimpleText:=' Название не удалось изменить';
+if (FileIndex[1]<>'0') and (FileIndex[1]<>'1') then Delete(FileIndex,1,1);
+if InputQuery(Application.Title, 'Введите новое название для '+FileIndex, NewName) then
+if Trim(NewName)<>'' then begin
+if FDevice.Rename(AnsiToUtf8(Path+'/'+FileIndex),AnsiToUtf8(Path+'/'+NewName)) then StatusBar1.SimpleText:=' Название успешно изменено' else StatusBar1.SimpleText:=' Название не удалось изменить';
 Dir(Path);
 end else StatusBar1.SimpleText:=' Неверно задано название';
 end;
 
 function BrowseFolderDialog(title:PChar):string;
 var
-titlename:string;
+TitleName:string;
 lpitemid:pitemidlist;
-browseinfo:tbrowseinfo;
-displayname:array[0..max_Path] of char;
-tempPath:array[0..max_Path] of char;
+BrowseInfo:TBrowseInfo;
+DisplayName:array[0..max_Path] of char;
+TempPath:array[0..max_Path] of char;
 begin
-fillchar(browseinfo,sizeof(tbrowseinfo),#0);
-browseinfo.hwndowner:=GetDesktopWindow;
-browseinfo.pszdisplayname:=@displayname;
-titlename:=title;
-browseinfo.lpsztitle:=PChar(titlename);
-browseinfo.ulflags:=bif_returnonlyfsdirs;
-lpitemid:=shbrowseforfolder(browseinfo);
-if lpitemid<>nil then begin
-shgetPathfromidlist(lpitemid, tempPath);
-result:=tempPath;
-globalfreeptr(lpitemid);
+FillChar(BrowseInfo,sizeof(tBrowseInfo),#0);
+BrowseInfo.hwndowner:=GetDesktopWindow;
+BrowseInfo.pszdisplayname:=@displayname;
+TitleName:=title;
+BrowseInfo.lpsztitle:=PChar(TitleName);
+BrowseInfo.ulflags:=bIf_ReturnOnlyFSDirs;
+lpItemId:=shBrowseForFolder(BrowseInfo);
+if lpItemId<>nil then begin
+shGetPathFromIdList(lpItemId, TempPath);
+Result:=TempPath;
+GlobalFreePtr(lpitemid);
 end;
 end;
 
-function DownloadFile(address,LocalPath:string):boolean;
+function DownloadFile(Address,LocalPath:string):boolean;
 var
 RemoteFileSize:Int64;
 AMobileFile:TAMobileDeviceFileStream;
 LocalFile:TFileStream;
 begin
-RemoteFileSize:=Form1.FDevice.GetFileSize(address);
-LocalFile:=TFileStream.Create(LocalPath+'\'+ExtractFileName(StringReplace(address,'/','\',[rfReplaceAll])),fmCreate);
+RemoteFileSize:=Form1.FDevice.GetFileSize(Address);
+LocalFile:=TFileStream.Create(LocalPath+'\'+ExtractFileName(StringReplace(Address,'/','\',[rfReplaceAll])),fmCreate);
 try
-AMobileFile:=TAMobileDeviceFileStream.Create(Form1.FDevice,address,omRead);
+AMobileFile:=TAMobileDeviceFileStream.Create(Form1.FDevice,Address,omRead);
 AMobileFile.OnProgressStep:=Form1.DoOnFileTransStep;
 AMobileFile.Position:=0;
 LocalFile.CopyFrom(AMobileFile,RemoteFileSize);
@@ -448,26 +432,26 @@ Result:=false;
 end;
 end;
 
-function DownloadDir(address,LocalPath:string):boolean;
+function DownloadDir(Address,LocalPath:string):boolean;
 var
 DirList:TStringList; i:integer;
 begin
 try
-address:=StringReplace(UTF8ToAnsi(address),'и?','й',[rfreplaceall]);
-address:=AnsiToUTF8(StringReplace(address,'И?','Й',[rfreplaceall]));
+Address:=StringReplace(UTF8ToAnsi(Address),'и?','й',[rfReplaceAll]);
+Address:=AnsiToUTF8(StringReplace(Address,'И?','Й',[rfReplaceAll]));
 
-CreateDir(LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(address,'/','\',[rfReplaceAll]))));
+CreateDir(LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(Address,'/','\',[rfReplaceAll]))));
 DirList:=TStringList.Create;
 
-Form1.FDevice.GetFiles(address,DirList);
+Form1.FDevice.GetFiles(Address,DirList);
 for i:=0 to DirList.Count-1 do
-DownloadFile(address+'/'+DirList.Strings[i],LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(address,'/','\',[rfReplaceAll]))));
+DownloadFile(Address+'/'+DirList.Strings[i],LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(Address,'/','\',[rfReplaceAll]))));
 
 DirList.Clear;
 
-Form1.FDevice.GetDirectories(address,DirList);
+Form1.FDevice.GetDirectories(Address,DirList);
 for i:=0 to DirList.Count-1 do
-if (DirList.Strings[i]<>'.') and (DirList.Strings[i]<>'..') then DownloadDir(address+'/'+DirList.Strings[i],LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(address,'/','\',[rfReplaceAll]))));
+if (DirList.Strings[i]<>'.') and (DirList.Strings[i]<>'..') then DownloadDir(Address+'/'+DirList.Strings[i],LocalPath+'\'+UTF8ToAnsi(ExtractFileName(StringReplace(Address,'/','\',[rfReplaceAll]))));
 
 DirList.Free;
 Result:=true;
@@ -483,22 +467,17 @@ begin
 TempPath:=BrowseFolderDialog('Выберите каталог');
 if TempPath<>'' then begin
 if FileIndex[1]<>'2' then begin
-delete(FileIndex,1,1);
+Delete(FileIndex,1,1);
 StatusBar1.SimpleText:=' Идет копирование файла';
 if DownloadFile(AnsiToUtf8(Path+'/'+FileIndex),TempPath) then StatusBar1.SimpleText:=' Файл успешно скопирован' else
 StatusBar1.SimpleText:=' В процессе копирования произошла ошибка';
 end else begin
-delete(FileIndex,1,1);
+Delete(FileIndex,1,1);
 StatusBar1.SimpleText:=' Идет копирование файлов';
 if DownloadDir(AnsiToUtf8(Path+'/'+FileIndex),TempPath) then StatusBar1.SimpleText:=' Копирование успешно завершено' else
 StatusBar1.SimpleText:=' В процессе копирования произошла ошибка';
 end;
 end else StatusBar1.SimpleText:=' Не выбран каталог';
-end;
-
-procedure TForm1.StatusBar(SimpleText: string);
-begin
-
 end;
 
 end.
